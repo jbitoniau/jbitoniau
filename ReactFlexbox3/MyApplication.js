ba67fd83d996f3db1fe4fbaa4afd2176f4a7a821
jbitoniau <jmenuet@gmail.com>
2015-09-23 18:44:44 +0200

    Creating a control which is more flexible in its handling of size/layout

--- ReactFlexbox3/MyApplication.js
+++ ReactFlexbox3/MyApplication.js
@@ -26,21 +26,25 @@ var MyApplication = React.createClass
 						
 							display:'flex',
 							justifyContent: 'space-between',
-							alignItems: 'stretch',
+							alignItems: 'stretch',		// 'flex-start'
 							flexWrap: 'wrap',
 							flexDirection:'column',
 							backgroundColor:'rgba(255, 0, 0, 0.2)'}}, 
 				 
 							React.createElement("img", {src: "1.png", style: {flex:'none', height:50}}), 
-							React.createElement("img", {src: "2.png", style: {flex:'auto', minHeight:100, maxHeight:400}, ref: "deux"}), 
+							React.createElement("img", {src: "2.png", style: {flex:'none', minHeight:50, maxHeight:200}, ref: "deux"}), 
 							React.createElement(MySlider, {style: {flex:'none', backgroundColor:'#FF00FF'}, barWidth: 300}), 
 							React.createElement("img", {src: "3.png", style: {flex:'none', height:50}}), 
-							React.createElement(MyButton, {style: {flex:'none', backgroundColor:'#FF00FF', boxShadow:' 8px 8px 0px #000'}, image: "ToolAddVoxel.png", width: 128, height: 128})
-							
+							React.createElement(MyButton, {style: {flex:'none', backgroundColor:'#FF00FF', boxShadow:' 8px 8px 0px #000'}, image: "ToolAddVoxel.png", width: 128, height: 128}), 
+							React.createElement("img", {src: "1.png", style: {flex:'none', height:50}}), 
+							React.createElement(MyTestControl, {style: {flex:'auto', backgroundColor:'magenta'}}), 
+							React.createElement(MyTestControl, {style: {flex:'none', width:300, alignSelf:'flex-end', backgroundColor:'magenta'}})
 						)
 					)
 				);
 		},
+		
+		/* <MyTestControl style={{flex:'auto', backgroundColor:'#FF00FF', boxShadow:'8px 8px 0px #000'}} /> */
 		
 		onClick: function(e)
 		{	
--- ReactFlexbox3/MyApplication.jsx
+++ ReactFlexbox3/MyApplication.jsx
@@ -26,21 +26,25 @@ var MyApplication = React.createClass
 						
 							display:'flex',
 							justifyContent: 'space-between',
-							alignItems: 'stretch',
+							alignItems: 'stretch',		// 'flex-start'
 							flexWrap: 'wrap',
 							flexDirection:'column',
 							backgroundColor:'rgba(255, 0, 0, 0.2)'}}>
 				 
 							<img src='1.png' style={{flex:'none', height:50}}/>
-							<img src='2.png' style={{flex:'auto', minHeight:100, maxHeight:400}} ref='deux'/>
+							<img src='2.png' style={{flex:'none', minHeight:50, maxHeight:200}} ref='deux'/>
 							<MySlider style={{flex:'none', backgroundColor:'#FF00FF'}} barWidth={300}/>
 							<img src='3.png' style={{flex:'none', height:50}}/>
 							<MyButton style={{flex:'none', backgroundColor:'#FF00FF', boxShadow:' 8px 8px 0px #000'}} image='ToolAddVoxel.png' width={128} height={128}/>
-							
+							<img src='1.png' style={{flex:'none', height:50}}/>
+							<MyTestControl style={{flex:'auto', backgroundColor:'magenta'}}/>
+							<MyTestControl style={{flex:'none', width:300, alignSelf:'flex-end', backgroundColor:'magenta'}}/>
 						</div>
 					</div>
 				);
 		},
+		
+		/* <MyTestControl style={{flex:'auto', backgroundColor:'#FF00FF', boxShadow:'8px 8px 0px #000'}} /> */
 		
 		onClick: function(e)
 		{	
